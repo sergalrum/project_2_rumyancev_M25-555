c9a80231057a263f23455f4d70e8d58f4f85f911
Sergey Rumyancev <133sergey@gmail.com>
2025-11-05 11:45:28 +0300

    Обновить utils.py: добавить create_cacher и утилиты

--- src/primitive_db/utils.py
+++ src/primitive_db/utils.py
@@ -18,3 +18,21 @@ def save_table_data(table_name, data):
     file_path = DATA_DIR / f"{table_name}.json"
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+def create_cacher():
+    """Создаёт кэш-функцию с замыканием."""
+    cache = {}
+    
+    def cache_result(key, value_func):
+        """
+        Проверяет наличие результата в кэше.
+        Если есть — возвращает его, иначе вызывает value_func и сохраняет результат.
+        """
+        if key in cache:
+            return cache[key]
+        else:
+            result = value_func()
+            cache[key] = result
+            return result
+    
+    return cache_result
